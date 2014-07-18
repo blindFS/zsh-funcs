2e74d48b5d9071cd268d081ffe95c87c2a4ae41d
farseer90718 <farseer90718@gmail.com>
2014-07-18 10:08:12 +0800

    update

--- repo-update.zsh
+++ repo-update.zsh
@@ -25,6 +25,8 @@ function src-update () {
 }
 
 function repo-update {
+    cd $HOME
+    git submodule foreach git pull --rebase
     print "\n\33[32m*************************\33[34mnpm\33[32m**********************************\33[0m\n"
     sudo npm update -g
     print "\n\33[32m*************************\33[34mgem\33[32m**********************************\33[0m\n"
@@ -38,15 +40,5 @@ function repo-update {
         src-update $HOME/src
     else
         echo "$HOME/src not exist"
-    fi
-}
-
-function peel () {
-read -q "ans?Are you sure you want to mv everything in $1 to current folder and delete $1? (y/n)"
-    if [[ $ans == "y" ]]; then
-        cd $1
-        mv * ..
-        cd ..
-        rm -rf $1
     fi
 }
